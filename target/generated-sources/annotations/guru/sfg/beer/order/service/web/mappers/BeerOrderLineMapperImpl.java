62298405c63c6c36046c66564263f0b2d5825b8b
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-09 20:51:53 +0530

    adding port number

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-09T18:59:29+0530",
+    date = "2021-12-09T19:07:03+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
