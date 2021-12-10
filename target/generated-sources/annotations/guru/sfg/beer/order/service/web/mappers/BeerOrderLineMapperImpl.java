15bf59547dcad6bff4f9769d841dab7cf4a04dda
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-10 18:17:42 +0530

    updating project

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-10T18:09:03+0530",
+    date = "2021-12-10T18:15:26+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
