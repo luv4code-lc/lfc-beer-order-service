c67f0bd558754c8deb6e3620155c91cd874ae215
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-10 14:38:38 +0530

    beer order service not saving upc

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-10T12:59:28+0530",
+    date = "2021-12-10T14:37:49+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.12 (Oracle Corporation)"
 )
 @Component
